ce20e5a1974ec25418b58bdaf00015d3bd145c65
AnCa1228 <andrescamiloarizavargas@gmail.com>
2023-04-25 19:22:29 -0500

    primer commit

---
+++ src/main/java/com/usa/primarapp/PrimarappApplication.java
@@ -0,0 +1,13 @@
+package com.usa.primarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimarappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usa/primarapp/PrimarappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usa.primarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
